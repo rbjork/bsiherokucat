22927c8ba4930f3a24c04f62ae2eef7608f8b082
Ron <grbtxtmsg@gmail.com>
2022-04-13 09:36:49 -0700

    App full load

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def bsiquantarium():
 
 @app.route('/', methods=['GET','POST'])
 def bsicatalog():
-	return render_template("parcelcatalogcontainer.html")
+	return render_template("parcelcatalog.html")
 
 @app.route("/getbody", methods=['GET'])
 def getbody():
